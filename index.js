4bc0f5928ee130e08ea63b44940d13b2b749268e
Juan <jnvleite@gmail.com>
2019-08-09 19:32:27 -0300

    enable yellowbox again because its good actually

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ import { AppRegistry } from 'react-native';
 import App from '~'
 import { name as appName } from './app.json';
 
-console.disableYellowBox = true;
-
 AppRegistry.registerComponent(appName, () => App);
